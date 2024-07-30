d5da665d50bd278fd776f63eb3c79635cf6b82bd
Delvis Soto <delvissoto99@icloud.com>
2024-07-30 15:19:21 -0500

    Python Data Structure

---
+++ kk.py
@@ -0,0 +1,25 @@
+# Creating a dictionary
+my_dict = {'name': 'Alice', 'age': 25, 'city': 'Wonderland'}
+
+# Accessing a value
+print("Name:", my_dict['name'])
+
+# Adding a new key-value pair
+my_dict['gender'] = 'Female'
+
+# Updating a value
+my_dict['age'] = 26
+
+# Deleting a key-value pair
+del my_dict['city']
+
+# Printing the dictionary
+print("Current Dictionary:", my_dict)
+
+
+
+
+s.add(4)
+s.add(4)
+s.add(5)
+s.remove(3)
